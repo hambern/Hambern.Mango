ab3c63c89c90574a45ea601e30cdda2765586ed0
Mattias Hamberg <mattias@kretiochpleti.se>
2016-12-14 18:15:51 +0100

    Added gumshoe and stuff

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     new WOW().init();
     plyr.setup();
     smoothScroll.init();
+    gumshoe.init();
     $('.image-link').magnificPopup({type: 'image'});
     $('.slick-carousel').slick();
 });
